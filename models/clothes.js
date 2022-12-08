bdbeba624b145e2fed8bc7267e6e394e2ad1cf56
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-12-08 11:31:32 -0600

    put some properties in the schema. Still trying to figure out all we will need for the clothing schema

--- models/clothes.js
+++ models/clothes.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose')
 
-const clothingSchema = new mongoose.Schema {(
+
+// still trying to figure out the schema
+const clothingSchema = new mongoose.Schema ({
     name: String,
     image: String,
     price: String,
+    rgbcolors: [String],
+    catName: String,
+    articleColorNames: [String],
 
-)}
+})
+
+
+const Clothes = mongoose.model('Cloth', clothingSchema)
+// module.exports = Clothes 
